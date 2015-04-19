9336b80d1a7af6d0d682ef9309380830976a2110
mlgreen <michellegreentx@gmail.com>
2015-04-19 14:00:44 -0500

    practiced making functions

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -4,4 +4,27 @@
 # 4.19.15
 
 # read in data as tab-delimited
-gap.in <- read.table("output/combined_gapMinder.tsv", sep="\t")
+gap.in <- read.table("output/combined_gapMinder.tsv", sep="\t", header=TRUE)
+
+for (number in 1:10){
+  print(number + 10)
+}
+
+for (number in 1:10){
+  number.out <- number + 10
+}
+
+for (number in 1:10){
+  print(gap.in[number,])
+}
+
+for (row.number in 1:10){
+  for (col.number in 1:5){print(gap.in[row.number, col.number])}
+}
+
+#make a function
+add.me <- function(x,y){
+  x + y
+}
+
+add.me(3,4)
