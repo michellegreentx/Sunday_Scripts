fe16182d5aa44e540eefe93ec9970c89e9b39089
mlgreen <michellegreentx@gmail.com>
2015-04-19 14:51:30 -0500

    did some sorting and did exercise before coffee break

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -30,7 +30,32 @@ add.me <- function(x,y){
 add.me(3,4)
 
 #installing packages
-install.packages("dplyr", dependencies=TRUE)
+# (now that package is installed, change script line to comment so that it doesn't install again) 
+#install.packages("dplyr", dependencies=TRUE)
 
 #installed but not loaded yet, so still need to load
 library("dplyr")
+
+#using pipe to ask it to filter for pops over 15 mil and then select only the country, year
+# and pop to display
+#full code gives you the values for the years in which the pop was the lowest 
+#(yet still above 15 mil)
+gap.in %>%
+  filter(pop > 15000000) %>%
+  select(country, year, pop) %>%
+  group_by(country) %>%
+  summarize(min = min(pop))
+
+#calculate the mean population per continent per year for years prior to 1990
+gap.in %>%
+  filter(year<1990) %>%
+  select(continent,pop, year) %>%
+  group_by(continent, year) %>%
+  summarize(mean(pop), trim=0, na.rm=FALSE) %>%
+  as.data.frame()
+
+gap.in %>%
+  filter(year<1990) %>%
+  select(continent,pop, year) %>%
+  group_by(continent, year) %>%
+  summarize(mean(pop), trim=0, na.rm=FALSE) -> data.out
